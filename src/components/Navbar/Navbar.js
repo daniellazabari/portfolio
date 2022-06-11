ab4eed2000ff493f960f7549cc87ee3604d74780
daniella.zabari <daniella.zabari@post.idc.ac.il>
2022-06-11 14:25:50 +0300

    Added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { logo } from './logo.svg';
+import logo from './logo.svg';
+import { NavLink } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className={styles.navbar}>
+      <NavLink to='/'>
+        <img src={logo} className="App-logo" alt="logo" />
+      </NavLink>
+      <NavLink to='About'>About</NavLink>
+      <NavLink to='Projects'>Projects</NavLink>
+      <NavLink to='Contact'>Contact</NavLink>
     </div>
   )
 }
